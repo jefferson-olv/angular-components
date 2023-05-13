e4616c5731ee484bc313531b1652cb285a9bc797
jefferson-olv <78991983+jefferson-olv@users.noreply.github.com>
2023-05-12 23:36:59 -0300

    3 files modified

--- src/app/component/entry-data.component.ts
+++ src/app/component/entry-data.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./entry-data.component.css']
 })
 export class EntryDataComponent {
-  
+  placeholder:string = 'email'
+  placepass:string = 'senha'
+  color:string = 'red'
 }
